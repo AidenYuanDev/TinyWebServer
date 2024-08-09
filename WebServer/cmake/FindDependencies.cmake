0949b0ef42a2c0185b904d1e6a76b13a27d23b62
aiden <aiden.yuan.dev@gmail.com>
2024-08-08 22:49:48 -0400

    使用pkg-config查找依赖

--- WebServer/cmake/FindDependencies.cmake
+++ WebServer/cmake/FindDependencies.cmake
@@ -14,17 +14,23 @@
 # set(BUILD_GTEST ON CACHE BOOL "" FORCE)
 # FetchContent_MakeAvailable(googletest)
 
-include(FetchContent)
+# include(FetchContent)
+#
+# FetchContent_Declare(
+#   yaml-cpp
+#   GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+#   GIT_TAG master # Can be a tag (yaml-cpp-x.x.x), a commit hash, or a branch name (master)
+# )
+# FetchContent_GetProperties(yaml-cpp)
+#
+# if(NOT yaml-cpp_POPULATED)
+#   message(STATUS "Fetching yaml-cpp...")
+#   FetchContent_Populate(yaml-cpp)
+#   add_subdirectory(${yaml-cpp_SOURCE_DIR} ${yaml-cpp_BINARY_DIR})
+# endif()
 
-FetchContent_Declare(
-  yaml-cpp
-  GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
-  GIT_TAG master # Can be a tag (yaml-cpp-x.x.x), a commit hash, or a branch name (master)
-)
-FetchContent_GetProperties(yaml-cpp)
+# 查找PkgConfig包
+find_package(PkgConfig REQUIRED)
 
-if(NOT yaml-cpp_POPULATED)
-  message(STATUS "Fetching yaml-cpp...")
-  FetchContent_Populate(yaml-cpp)
-  add_subdirectory(${yaml-cpp_SOURCE_DIR} ${yaml-cpp_BINARY_DIR})
-endif()
+# 使用PkgConfig查找yaml-cpp
+pkg_check_modules(YAML_CPP REQUIRED yaml-cpp)
